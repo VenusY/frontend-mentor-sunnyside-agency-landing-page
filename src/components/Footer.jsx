815057a0a914cdb4ec7ea6195e00e8627a9b9ce3
Venus <venusyip@outlook.com>
2025-04-03 18:33:28 +0100

    Add Footer component markup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,59 @@
-export default function Footer() {}
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <img
+        src={require('../images/logo.svg')}
+        alt='Logo'
+        className='footer__logo'
+      />
+
+      <nav className='footer__nav'>
+        <a href='#' className='footer__page-link'>
+          About
+        </a>
+
+        <a href='#' className='footer__page-link'>
+          Services
+        </a>
+
+        <a href='#' className='footer__page-link'>
+          Projects
+        </a>
+      </nav>
+
+      <nav className='footer__socials'>
+        <a href='#' className='footer__social-link'>
+          <img
+            src={require('../images/icon-facebook.svg')}
+            alt='Facebook'
+            className='footer__social-icon'
+          />
+        </a>
+
+        <a href='#' className='footer__social-link'>
+          <img
+            src={require('../images/icon-instagram.svg')}
+            alt='Instagram'
+            className='footer__social-icon'
+          />
+        </a>
+
+        <a href='#' className='footer__social-link'>
+          <img
+            src={require('../images/icon-twitter.svg')}
+            alt='Twitter'
+            className='footer__social-icon'
+          />
+        </a>
+
+        <a href='#' className='footer__social-link'>
+          <img
+            src={require('../images/icon-pinterest.svg')}
+            alt='Pinterest'
+            className='footer__social-icon'
+          />
+        </a>
+      </nav>
+    </footer>
+  );
+}
