c49f3133d9c4981708924b025f4ac65b0859b451
Venus <venusyip@outlook.com>
2025-04-03 19:26:55 +0100

    Add file extensions to image file paths

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,43 +2,43 @@ export default function Gallery() {
   return (
     <div className='gallery'>
       <img
-        src={require('../images/mobile/image-gallery-milkbottles')}
+        src={require('../images/mobile/image-gallery-milkbottles.jpg')}
         alt='Milk bottles'
         className='gallery__image  gallery__image--mobile'
       />
       <img
-        src={require('../images/mobile/image-gallery-orange')}
+        src={require('../images/mobile/image-gallery-orange.jpg')}
         alt='Orange on a plate'
         className='gallery__image  gallery__image--mobile'
       />
       <img
-        src={require('../images/mobile/image-gallery-cone')}
+        src={require('../images/mobile/image-gallery-cone.jpg')}
         alt='Ice cream cone'
         className='gallery__image  gallery__image--mobile'
       />
       <img
-        src={require('../images/mobile/image-gallery-sugar-cubes')}
+        src={require('../images/mobile/image-gallery-sugar-cubes.jpg')}
         alt='Sugar cubes'
         className='gallery__image  gallery__image--mobile'
       />
 
       <img
-        src={require('../images/desktop/image-gallery-milkbottles')}
+        src={require('../images/desktop/image-gallery-milkbottles.jpg')}
         alt='Milk bottles'
         className='gallery__image  gallery__image--desktop'
       />
       <img
-        src={require('../images/desktop/image-gallery-orange')}
+        src={require('../images/desktop/image-gallery-orange.jpg')}
         alt='Orange on a plate'
         className='gallery__image  gallery__image--desktop'
       />
       <img
-        src={require('../images/desktop/image-gallery-cone')}
+        src={require('../images/desktop/image-gallery-cone.jpg')}
         alt='Ice cream cone'
         className='gallery__image  gallery__image--desktop'
       />
       <img
-        src={require('../images/desktop/image-gallery-sugar-cubes')}
+        src={require('../images/desktop/image-gallery-sugar-cubes.jpg')}
         alt='Sugar cubes'
         className='gallery__image  gallery__image--desktop'
       />
