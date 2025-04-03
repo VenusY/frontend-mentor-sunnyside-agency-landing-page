ddd3fb13a8c2a9db17afcb26dfacda3c306be9ca
Venus <venusyip@outlook.com>
2025-04-03 18:25:28 +0100

    Add Header component markup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,29 @@
-export default function Header() {}
+export default function Header() {
+  return (
+    <header className='header'>
+      <nav className='nav-bar'>
+        <a href='' className='logo__link'>
+          <img src={require('../images/logo.svg')} alt='' className='logo' />
+        </a>
+
+        <div className='links'>
+          <a href='#' className='links__page'>
+            About
+          </a>
+          <a href='#' className='links__page'>
+            Services
+          </a>
+          <a href='#' className='links__page'>
+            Projects
+          </a>
+          <button className='links__contact-button'>Contact</button>
+        </div>
+      </nav>
+
+      <section className='intro'>
+        <h1 className='intro__heading'>We Are Creatives</h1>
+        <div className='intro__arrow'></div>
+      </section>
+    </header>
+  );
+}
